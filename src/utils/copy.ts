0898ea357eb04f36ba8b8d026e3be416b129a00e
qxieyong <143081419+qxieyong@users.noreply.github.com>
2024-05-14 18:41:45 +0800

    Update copy.ts

--- src/utils/copy.ts
+++ src/utils/copy.ts
@@ -1,18 +1,22 @@
-// import { Toast } from 'vant';
-// import { ElMessage } from 'element-plus';
 import ElMessage from './ElementUIMsg';
 import i18n from '@/utils/language';
 const { t } = i18n.global;
 const copy = (val: string) => {
-    let input: HTMLInputElement = document.createElement('input');
-    input.value = val;
-    document.body.appendChild(input);
-    input.select();
-    document.execCommand('Copy');
-    document.body.removeChild(input);
-    ElMessage.success({ message: `copy success`, duration: 1500 });
-    // ElMessage.success({ message: '复制成功！', duration: 1000 });
-    // alert('复制成功');
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(val).then(_res => {
+            ElMessage.success(t("header.inviteCopy"));
+        }).catch(_e => {
+            ElMessage.error(t("global.error"));
+        })
+    } else {
+        let input: HTMLInputElement = document.createElement('input');
+        input.value = val;
+        document.body.appendChild(input);
+        input.select();
+        document.execCommand('Copy');
+        document.body.removeChild(input);
+        ElMessage.success(t("header.inviteCopy"));
+    }
 };
 
-export default  copy;
+export default copy;

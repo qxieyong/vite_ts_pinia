5b80147b630dca6efd36c73d62ea608733e657e2
xieyong <1239665882@qq.com>
2024-05-30 18:52:34 +0800

    eslint 代码要求

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,65 +1,65 @@
 // src/axios.ts
-import axios from 'axios';
+import axios from "axios";
 
-//使用create方法创建axios实例
+// 使用create方法创建axios实例
 const service = axios.create({
-  baseURL: '/chat',
-  // timeout: 20000, // 请求超时时间
+	baseURL: "/chat"
+	// timeout: 20000, // 请求超时时间
 });
 
 // 添加请求拦截器
 service.interceptors.request.use(
-  (config) => {
-    let configs: any = config;
-    if (configs.url.includes('questionAnswer')) {
-      configs.headers['Content-Type'] = 'multipart/form-data';
-    } else {
-      configs.headers['Content-Type'] = 'application/json;charset=utf-8';
-    }
-    let token;
-    configs.headers.token = token;
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+	config => {
+		const configs: any = config;
+		if (configs.url.includes("questionAnswer")) {
+			configs.headers["Content-Type"] = "multipart/form-data";
+		} else {
+			configs.headers["Content-Type"] = "application/json;charset=utf-8";
+		}
+		let token;
+		configs.headers.token = token;
+		return config;
+	},
+	error => {
+		return Promise.reject(error);
+	}
 );
 
 export function axiosPost(url: string, params: any = {}) {
-  return new Promise((resolve, reject) => {
-    service
-      .post(url, params)
-      .then((response) => {
-        resolve(response.data || response);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+	return new Promise((resolve, reject) => {
+		service
+			.post(url, params)
+			.then(response => {
+				resolve(response.data || response);
+			})
+			.catch(error => {
+				reject(error);
+			});
+	});
 }
 
 export function axiosGet(url: string, params?: any) {
-  return new Promise((resolve, reject) => {
-    if (params) {
-      service
-        .get(url, { params })
-        .then((response) => {
-          resolve(response?.data || response);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    } else {
-      service
-        .get(url)
-        .then((response) => {
-          resolve(response?.data || response);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    }
-  });
+	return new Promise((resolve, reject) => {
+		if (params) {
+			service
+				.get(url, { params })
+				.then(response => {
+					resolve(response?.data || response);
+				})
+				.catch(error => {
+					reject(error);
+				});
+		} else {
+			service
+				.get(url)
+				.then(response => {
+					resolve(response?.data || response);
+				})
+				.catch(error => {
+					reject(error);
+				});
+		}
+	});
 }
 
 export default service;
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,12 +1,7 @@
 <template>
-  <div>
-  </div>
+	<div></div>
 </template>
 
-<script lang='ts' setup>
+<script lang="ts" setup></script>
 
-</script>
-
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
